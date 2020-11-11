0ada8a43c14a91caa2f6da56a11e8e4ae5261ea9
DanielArruda98 <arruda.daniel20@gmail.com>
2020-11-11 00:03:45 -0300

    version links comentados

--- frontend/js/menu/links.js
+++ frontend/js/menu/links.js
@@ -20,11 +20,10 @@ function startPagina(event = null) {
     redirect(event, ".link_home", "home.html", "Series Manager");
     selecionarLink('.link_home');
 
-    window.history.pushState(state, title, 'home');
+    // window.history.pushState({}, null, 'home');
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////////////////
 startPagina();
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -33,17 +32,12 @@ $('.link_home').click(function(event) {
     startPagina(event);
 });
 
-$('.link_usuarios').click(function(event) {
-    redirect(event, this, "usuarios.html", "Series Manager: Usuários");
-    window.history.pushState(state, title, 'usuarios');
-});
-
 $('.link_filmes').click(function(event) {
     redirect(event, this, "filmes.html", "Series Manager: Filmes");
-    window.history.pushState(state, title, 'filmes');
+    // window.history.pushState({}, null, 'filmes');
 });
 
 $('.link_series').click(function(event) {
     redirect(event, this, "series.html", "Series Manager: Séries");
-    window.history.pushState(state, title, 'series');
+    // window.history.pushState({}, null, 'series');
 });
